5f1d53343bb0ad28e5bc4755a22876be88f44b36
RiverRobo <101362583+RiverRobo@users.noreply.github.com>
2022-05-05 13:21:25 -0700

    Add files via upload

---
+++ games-database/Advancement.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+    sandbox(int16_value, double_value, datetime_value)
+VALUES
+    (-235, 1557.4657, '2030-02-15 13:18:56')
+
+UPDATE
+    sandbox 
+SET 
+    int32_value = 781650874, 
+    bool_value
